9b74f4db92f18c2ef0f02d72bc94245622e59dad
doebi <doebi@devlol.org>
2015-01-08 02:10:31 +0100

    finished implementation of help command

--- modules/help.py
+++ modules/help.py
@@ -2,42 +2,46 @@
 # -*- coding: utf-8 -*-
 import willie
 
-commands = [
-    {
-        'synopsis': 'help',
+commands = {
+    'help': {
+        'synopsis': 'help [ COMMAND ]',
         'description': 'Prints this manual.',
         'alias': ['h', 'man'],
     },
-    {
+    'status': {
         'synopsis': 'status [ OPEN | CLOSED ]',
         'description': 'Replies the current Status of the Hackerspace-API or sets it to the provided State either OPEN or CLOSED',
         'alias': ['s']
     },
-    {
-        'synopsis': 'weather',
-        'description': 'Prints a human-readable summary of the hourly weather-data provided by forecast.io for Linz.',
-        'alias': ['w'],
-    },
-    {
-        'synopsis': 'fuckingweather',
-        'description': 'Prints the fucking weather.',
-        'alias': ['fw'],
-    },
-    {
-        'synopsis': 'temp',
-        'description': 'Prints current temperature and how it feels like.',
-        'alias': ['t'],
-    },
-    {
+    'devmon': {
         'synopsis': 'devmon',
         'description': 'Replies statistics of collected meta-data about our network-infrastructure.',
         'alias': ['dm', 'mon'],
     },
-]
+    'door': {
+        'synopsis': 'door',
+        'description': 'Prints the current status of the door at /dev/lol. If locked or unlocked.',
+        'alias': ['dm', 'mon'],
+    },
+    'temp': {
+        'synopsis': 'temp',
+        'description': 'Prints current temperature and how it feels like.',
+        'alias': ['t'],
+    },
+    'weather': {
+        'synopsis': 'weather',
+        'description': 'Prints a human-readable summary of the hourly weather-data provided by forecast.io for Linz.',
+        'alias': ['w'],
+    },
+}
 
 @willie.module.commands('h', 'help', 'man')
 def help(bot, trigger):
-    dump = ""
-    dump += "=== /dev/bot manual ==="
-    dump += "# Commands"
-    bot.reply(dump)
+    cmd = trigger.group(2)
+    if cmd:
+        bot.say('.' + commands[cmd]['synopsis'])
+        bot.say(commands[cmd]['description'])
+    else:
+        bot.say("=== /dev/bot manual ===")
+        for k, v in sorted(commands.iteritems()):
+            bot.say('.' + v['synopsis'])
