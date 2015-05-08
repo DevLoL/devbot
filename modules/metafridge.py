c598ad2e12502fe5c48ef872d411ea22d73aeb22
doebi <doebi@devlol.org>
2015-05-08 02:49:58 +0200

    delted metafridge

--- modules/metafridge.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-import willie
-import urllib
-
-@willie.module.commands('metafridge')
-def metafridge(bot, trigger):
-    urllib.urlopen("http://metafridge.metalab.at/cgi-bin/post_text.cgi?killmockingbird=0&effect=0&scroll_text=" + trigger.group(2))
-    bot.reply("Posted to Metafridge!")
